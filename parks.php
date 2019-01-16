082813493bd559605d7ead85a8c610b59988bd19
LosBenos <losbenos@me.com>
2019-01-16 19:03:57 +0000

    Start Parks Page
    Added position: fixed to the nav list and style the layout a bit

--- parks.php
+++ parks.php
@@ -17,11 +17,11 @@
             <div class="parks-container">
                 <div class="park-list-container">
                     <ul class="park-list">
-                        <li><a href="">Disneyland</a></li>
-                        <li><a href="">Disney's Californian Adventure</a></li>
-                        <li><a href="">Magic Kingdom</a></li>
-                        <li><a href="">Epcot</a></li>
-                        <li><a href="">Hollywood Studios</a></li>
+                        <li><a href="#1">Disneyland</a></li>
+                        <li><a href="#2">Disney's Californian Adventure</a></li>
+                        <li><a href="#3">Magic Kingdom</a></li>
+                        <li><a href="#4">Epcot</a></li>
+                        <li><a href="#5">Hollywood Studios</a></li>
                         <li><a href="">Animal Kingdom</a></li>
                         <li><a href="">Typhoon Lagoon</a></li>
                         <li><a href="">Blizzard Beach</a></li>
@@ -34,27 +34,27 @@
             </div>
             <!--side-bar-->
 
-<div class="single-park-container">
+<div class="single-park-container" id="1">
+    <img src="img/Cinderellacastleblackandwhite.jpg" alt="">
+    <h2>DisneyLand</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores culpa soluta mollitia rem veniam ex, vel ipsam, qui. Quas, fugit, atque. Laboriosam tenetur omnis iure nesciunt earum eaque enim dolores.50</p>
+</div>
+<div class="single-park-container" id="2">
+    <img src="img/Cinderellacastleblackandwhite.jpg" alt="">
+    <h2>Californian</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores culpa soluta mollitia rem veniam ex, vel ipsam, qui. Quas, fugit, atque. Laboriosam tenetur omnis iure nesciunt earum eaque enim dolores.50</p>
+</div>
+<div class="single-park-container" id="3">
+    <img src="img/Cinderellacastleblackandwhite.jpg" alt="">
+    <h2>Magic</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores culpa soluta mollitia rem veniam ex, vel ipsam, qui. Quas, fugit, atque. Laboriosam tenetur omnis iure nesciunt earum eaque enim dolores.50</p>
+</div>
+<div class="single-park-container" id="4">
     <img src="img/Cinderellacastleblackandwhite.jpg" alt="">
     <h2>park name</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores culpa soluta mollitia rem veniam ex, vel ipsam, qui. Quas, fugit, atque. Laboriosam tenetur omnis iure nesciunt earum eaque enim dolores.50</p>
 </div>
-<div class="single-park-container">
-    <img src="img/Cinderellacastleblackandwhite.jpg" alt="">
-    <h2>park name</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores culpa soluta mollitia rem veniam ex, vel ipsam, qui. Quas, fugit, atque. Laboriosam tenetur omnis iure nesciunt earum eaque enim dolores.50</p>
-</div>
-<div class="single-park-container">
-    <img src="img/Cinderellacastleblackandwhite.jpg" alt="">
-    <h2>park name</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores culpa soluta mollitia rem veniam ex, vel ipsam, qui. Quas, fugit, atque. Laboriosam tenetur omnis iure nesciunt earum eaque enim dolores.50</p>
-</div>
-<div class="single-park-container">
-    <img src="img/Cinderellacastleblackandwhite.jpg" alt="">
-    <h2>park name</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores culpa soluta mollitia rem veniam ex, vel ipsam, qui. Quas, fugit, atque. Laboriosam tenetur omnis iure nesciunt earum eaque enim dolores.50</p>
-</div>
-<div class="single-park-container">
+<div class="single-park-container" id="5">
     <img src="img/Cinderellacastleblackandwhite.jpg" alt="">
     <h2>park name</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores culpa soluta mollitia rem veniam ex, vel ipsam, qui. Quas, fugit, atque. Laboriosam tenetur omnis iure nesciunt earum eaque enim dolores.50</p>
@@ -72,10 +72,6 @@
     <!-- primary ends !-->
 
 
-    <div class="secondary">
-        <!-- Sidebar Content -->
-    </div>
-    <!-- secondary -->
 
 
 </div>
