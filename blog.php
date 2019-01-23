00565c58e06562f3b6edebc375cdb27dcadbda05
LosBenos <losbenos@me.com>
2019-01-23 19:04:56 +0000

    Smooth scrolling and blog content

--- blog.php
+++ blog.php
@@ -1,7 +1,62 @@
 <?php include 'header.php' ?>
 
+<div class="site">
 
-<h2>Blog</h2>
+    <div class="site-content">
+
+        <div class="primary">
+
+
+<div class="blog-roll">
+    <single-entry>
+        <h2>Blog Title</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas at tempore sint. Itaque maiores autem magni nam minima, atque molestiae quos tempora! A voluptatibus aperiam possimus veritatis unde alias quibusdam tempora sunt libero ea fugit voluptatem, quo veniam maxime, repellendus vel iure ut sequi et ipsam asperiores beatae tenetur perspiciatis!</p>
+        <img src="http://lorempixel.com/400/200/" alt="">
+    </single-entry>
+    <single-entry>
+        <h2>Blog Title</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel quaerat alias illum dolorum autem consectetur non ullam, id voluptatibus accusamus maxime dignissimos, incidunt nihil fugit nostrum corporis officia pariatur nulla praesentium consequuntur, esse in deleniti possimus. Aperiam nam aspernatur, inventore odit iste repudiandae, accusantium ratione obcaecati. Repudiandae minus veniam optio.</p>
+        <img src="http://lorempixel.com/400/200/" alt="">
+    </single-entry>
+    <single-entry>
+        <h2>Blog Title</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque totam consequatur adipisci perspiciatis necessitatibus? Accusamus veniam at nulla dicta dolorum fugiat, sed, quae tempore odit, non mollitia sapiente, architecto possimus aut impedit reprehenderit! Distinctio deserunt repellendus vel aliquid pariatur aut! Fuga delectus dicta laboriosam mollitia temporibus, rem? Distinctio doloremque, eum?</p>
+        <img src="http://lorempixel.com/400/200/" alt="">
+    </single-entry>
+    <single-entry>
+        <h2>Blog Title</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi animi hic minima, cumque eum sint iure velit mollitia facere, maiores, fugiat obcaecati ullam, non magni nobis qui? Fugiat velit error expedita neque magnam alias facilis a. Nulla sit accusamus maxime sequi, odit sapiente velit, illo, nisi a nam, temporibus quo.</p>
+        <img src="http://lorempixel.com/400/200/" alt="">
+    </single-entry>
+    <single-entry>
+        <h2>Blog Title</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam laudantium tempora at eum. Atque perspiciatis quae nisi odit blanditiis deleniti minima deserunt repellat necessitatibus quaerat, repudiandae quo, aliquid, neque quisquam aspernatur dolor. Neque, eaque. Corrupti cum dicta sint nisi eligendi expedita ex unde, reprehenderit provident error voluptates magni dolorem consectetur.</p>
+        <img src="http://lorempixel.com/400/200/" alt="">
+    </single-entry>
+</div>
+
+
+
+
+        </div>
+        <!-- close primary -->
+
+
+        <div class="secondary">
+            <!-- sidebar content -->
+        </div>
+        <!-- secondary -->
+
+
+    </div>
+    <!-- close site content -->
+</div>
+<!-- close site-->
+
+
+
+
+
 
 
 
--- parks.php
+++ parks.php
@@ -34,22 +34,22 @@
             </div>
             <!--side-bar-->
 
-<div class="single-park-container" id="1">
+<div class="single-park-container section" id="1">
     <img src="img/Cinderellacastleblackandwhite.jpg" alt="">
     <h2>DisneyLand</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores culpa soluta mollitia rem veniam ex, vel ipsam, qui. Quas, fugit, atque. Laboriosam tenetur omnis iure nesciunt earum eaque enim dolores.50</p>
 </div>
-<div class="single-park-container" id="2">
+<div class="single-park-container section" id="2">
     <img src="img/Cinderellacastleblackandwhite.jpg" alt="">
     <h2>Californian</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores culpa soluta mollitia rem veniam ex, vel ipsam, qui. Quas, fugit, atque. Laboriosam tenetur omnis iure nesciunt earum eaque enim dolores.50</p>
 </div>
-<div class="single-park-container" id="3">
+<div class="single-park-container section" id="3">
     <img src="img/Cinderellacastleblackandwhite.jpg" alt="">
     <h2>Magic</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores culpa soluta mollitia rem veniam ex, vel ipsam, qui. Quas, fugit, atque. Laboriosam tenetur omnis iure nesciunt earum eaque enim dolores.50</p>
 </div>
-<div class="single-park-container" id="4">
+<div class="single-park-container section" id="4">
     <img src="img/Cinderellacastleblackandwhite.jpg" alt="">
     <h2>park name</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores culpa soluta mollitia rem veniam ex, vel ipsam, qui. Quas, fugit, atque. Laboriosam tenetur omnis iure nesciunt earum eaque enim dolores.50</p>
@@ -81,6 +81,81 @@
 <!-- site ends !-->
 
 
+
+<script>
+    //smooth scrolling
+    //TODO: add offset to the top when scrolling
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+</script>
+
+
+<script>
+    //this is scrollspy and doesnt work
+(function() {
+  'use strict';
+
+  var section = document.querySelectorAll(".section");
+  var sections = {};
+  var i = 0;
+
+  Array.prototype.forEach.call(section, function(e) {
+    sections[e.id] = e.offsetTop;
+  });
+
+  window.onscroll = function() {
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (i in sections) {
+      if (sections[i] <= scrollPosition) {
+        document.querySelector('.active').setAttribute('class', ' ');
+        document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+      }
+    }
+  };
+})();
+</script>
+
+<style>
+    /*this is the style for scrollspy*/
+    .active {
+        background-color: aquamarine;
+    }
+</style>
 
 
 <?php include 'footer.php' ?>
